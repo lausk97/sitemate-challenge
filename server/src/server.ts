a620972a259c665bd60473242adbeed6de000605
Alvin Lau <alvin_lau97@hotmail.com>
2024-06-10 15:45:17 +1000

    init client w/ typescript, tailwindcss, vite && server w/ typescript

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+mongoose.connect("mongodb://127.0.0.1/test");
+mongoose.connection.once("open", () => {
+  app.emit("ready");
+});
+
+app.on("ready", () => {
+  app.listen(process.env.PORT || 3001, () => {
+    console.log(`Listening to port ${process.env.PORT || 3001}`);
+  });
+});
